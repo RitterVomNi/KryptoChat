9aaf18ffa6b6e3851704abd6401aebeac0594ba7
RitterVomNi <heinen.m@web.de>
2016-07-15 07:58:41 +0200

    send messages to truby client

---
+++ app/src/main/java/team10/kryptochat/MySingleton.java
@@ -0,0 +1,32 @@
+package team10.kryptochat;
+
+/**
+ * Created by matthias on 15.07.16.
+ */
+public class MySingleton
+{
+    private static MySingleton instance;
+
+    public String customVar;
+
+    public static void initInstance()
+    {
+        if (instance == null)
+        {
+            // Create the instance
+            instance = new MySingleton();
+        }
+    }
+
+    public static MySingleton getInstance()
+    {
+        // Return the instance
+        return instance;
+    }
+
+    private MySingleton()
+    {
+        // Constructor hidden because this is a singleton
+    }
+
+}
